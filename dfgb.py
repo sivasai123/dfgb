0c86c08654886aa52fe98d8f2699e77df4c310a1
sivasai123 <52394434+sivasai123@users.noreply.github.com>
2019-07-31 12:35:03 +0530

    Create dfgb.py

---
+++ dfgb.py
@@ -0,0 +1,16 @@
+int main() 
+{ 
+    int n = 13, m = 4; 
+    cout << closestNumber(n, m) << endl; 
+      
+    n = -15; m = 6; 
+    cout << closestNumber(n, m) << endl; 
+      
+    n = 0; m = 8; 
+    cout << closestNumber(n, m) << endl; 
+      
+    n = 18; m = -7; 
+    cout << closestNumber(n, m) << endl; 
+      
+    return 0; 
+}  
